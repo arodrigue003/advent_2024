4d737d2318d4da5de89aa2bab3caf8c0ba51fe23
arodrigue003 <adrien.rod@aliceadsl.fr>
2024-12-19 23:02:31 +0100

    Perf: Improve day 19 performance

--- src/day19/logic.rs
+++ src/day19/logic.rs
@@ -1,20 +1,6 @@
-use crate::day19::models::Onsen;
 use hashbrown::HashSet;
-use itertools::Itertools;
-use regex::Regex;
 use std::cmp::min;
 
-pub fn solve_part_one(onsen: &Onsen) -> usize {
-    // Create a regex that can match
-    let towels_regex = Regex::new(&format!(r"^(?:{})*$", onsen.available_towels.iter().join("|"))).unwrap();
-
-    // Match designs
-    onsen
-        .target_designs
-        .iter()
-        .filter(|target| towels_regex.is_match(target))
-        .count()
-}
 
 pub fn count_combinations_rec(
     available_towels: &HashSet<String>,
@@ -55,15 +41,4 @@ pub fn count_combinations(available_towels: &HashSet<String>, design: &str, min_
     let mut cache = vec![usize::MAX; design.len()];
 
     count_combinations_rec(available_towels, design, min_size, max_size, &mut cache, 0)
-}
-
-pub fn solve_part_two(onsen: &Onsen) -> usize {
-    let min_size = onsen.available_towels.iter().map(|a| a.len()).min().unwrap();
-    let max_size = onsen.available_towels.iter().map(|a| a.len()).max().unwrap();
-
-    onsen
-        .target_designs
-        .iter()
-        .map(|design| count_combinations(&onsen.available_towels_set, design, min_size, max_size))
-        .sum()
 }
--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -5,7 +5,7 @@ mod parser;
 #[cfg(test)]
 mod tests;
 
-use crate::day19::logic::{solve_part_one, solve_part_two};
+use crate::day19::logic::{count_combinations};
 use crate::day19::models::Onsen;
 use crate::day19::parser::parse_input;
 use crate::models::AdventSolution;
@@ -13,6 +13,7 @@ use crate::models::AdventSolution;
 #[derive(Default)]
 pub struct Day19 {
     parsed_data: Option<Onsen>,
+    counts: Vec<usize>,
 }
 
 impl AdventSolution for Day19 {
@@ -20,11 +21,24 @@ impl AdventSolution for Day19 {
         self.parsed_data = Some(parse_input(_data));
     }
 
+    fn prepare(&mut self) {
+        let onsen = self.parsed_data.as_ref().unwrap();
+
+        let min_size = onsen.available_towels.iter().map(|a| a.len()).min().unwrap();
+        let max_size = onsen.available_towels.iter().map(|a| a.len()).max().unwrap();
+
+        self.counts = onsen
+            .target_designs
+            .iter()
+            .map(|design| count_combinations(&onsen.available_towels_set, design, min_size, max_size))
+            .collect();
+    }
+
     fn solve_part_one(&self) -> i128 {
-        solve_part_one(self.parsed_data.as_ref().unwrap()) as i128
+        self.counts.iter().filter(|count| **count != 0).count() as i128
     }
 
     fn solve_part_two(&self) -> i128 {
-        solve_part_two(self.parsed_data.as_ref().unwrap()) as i128
+        self.counts.iter().copied().sum::<usize>() as i128
     }
 }

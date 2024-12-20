b9d9e50472318ae769f070ae7a97450dc8f50366
arodrigue003 <adrien.rod@aliceadsl.fr>
2024-12-20 22:54:25 +0100

    Performance: Improve day 20 performance

--- src/day20/logic.rs
+++ src/day20/logic.rs
@@ -1,11 +1,9 @@
-use crate::day20::astar::astar;
 use crate::day20::models::{Map, Tile};
-use hashbrown::HashMap;
 use std::cmp::min;
 
 pub fn find_shortcuts(map: &Map, scores: &[Vec<usize>], max_size: usize) -> usize {
     // Store found shortcuts
-    let mut shortcuts: HashMap<usize, usize> = HashMap::new();
+    let mut result = 0;
 
     // For each point in the grind, find it's shortcuts
     for y in 0..map.height {
@@ -23,29 +21,18 @@ pub fn find_shortcuts(map: &Map, scores: &[Vec<usize>], max_size: usize) -> usiz
             for y_end in y_min..min(y + max_size + 1, map.height) {
                 for x_end in x_min..min(x + max_size + 1, map.width) {
                     let distance = y_end.abs_diff(y) + x_end.abs_diff(x);
-                    if distance <= max_size && map.grid[y_end][x_end] == Tile::Floor {
-                        let end_score = scores[y_end][x_end];
-                        if start_score + distance < end_score {
-                            // We found a shortcut
-                            *shortcuts.entry(end_score - start_score - distance).or_default() += 1;
-                        }
+                    let end_score = scores[y_end][x_end];
+                    if distance <= max_size
+                        && map.grid[y_end][x_end] == Tile::Floor
+                        && map.save_target + start_score + distance <= end_score
+                    {
+                        // We found a shortcut we are interested in
+                        result += 1;
                     }
                 }
             }
         }
     }
 
-    shortcuts.iter().filter(|(save, _)| **save >= map.save_target).map(|(_, count)| count).sum()
-}
-
-pub fn solve_part_one(map: &Map) -> usize {
-    let scores = astar(map).unwrap();
-
-    find_shortcuts(map, &scores, 2)
-}
-
-pub fn solve_part_two(map: &Map) -> usize {
-    let scores = astar(map).unwrap();
-
-    find_shortcuts(map, &scores, 20)
+    result
 }
--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -2,11 +2,12 @@ mod logic;
 mod models;
 mod parser;
 
+mod astar;
 #[cfg(test)]
 mod tests;
-mod astar;
 
-use crate::day20::logic::{solve_part_one, solve_part_two};
+use crate::day20::astar::astar;
+use crate::day20::logic::{find_shortcuts};
 use crate::day20::models::Map;
 use crate::day20::parser::parse_input;
 use crate::models::AdventSolution;
@@ -14,6 +15,7 @@ use crate::models::AdventSolution;
 #[derive(Default)]
 pub struct Day20 {
     parsed_data: Option<Map>,
+    scores: Option<Vec<Vec<usize>>>,
 }
 
 impl AdventSolution for Day20 {
@@ -21,11 +23,15 @@ impl AdventSolution for Day20 {
         self.parsed_data = Some(parse_input(_data));
     }
 
+    fn prepare(&mut self) {
+        self.scores = astar(self.parsed_data.as_ref().unwrap())
+    }
+
     fn solve_part_one(&self) -> i128 {
-        solve_part_one(self.parsed_data.as_ref().unwrap()) as i128
+        find_shortcuts(self.parsed_data.as_ref().unwrap(), self.scores.as_ref().unwrap(), 2) as i128
     }
 
     fn solve_part_two(&self) -> i128 {
-        solve_part_two(self.parsed_data.as_ref().unwrap()) as i128
+        find_shortcuts(self.parsed_data.as_ref().unwrap(), self.scores.as_ref().unwrap(), 20) as i128
     }
 }
